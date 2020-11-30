bcdc145a4bbf697743d47cfec16373f378b92989
handuy1992 <handuy1992@gmail.com>
2020-11-30 09:30:18 +0700

    serve static files

---
+++ render-html/serve-static-files/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/kataras/iris/v12"
+
+func main() {
+	app := iris.Default()
+	tmpl := iris.HTML("./view", ".html")
+	app.RegisterView(tmpl)
+
+	app.HandleDir("/resources", "./resources")
+
+	app.Get("/", func(ctx iris.Context){
+		ctx.View("home-page/index.html")
+	})
+
+	app.Listen(":8080")
+}
